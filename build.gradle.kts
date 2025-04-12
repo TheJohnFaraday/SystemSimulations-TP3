4113d77270a353dbfdad3666b5b37d51cc1a4868
lucky_number <jfernandezdinardo@itba.edu.ar>
2025-04-11 21:26:32 -0300

    Add dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.1.10"
+    id("application")
 }
 
 group = "ar.edu.itba.ss"
@@ -10,6 +11,14 @@ repositories {
 }
 
 dependencies {
+    // Clickt for CLI parsing
+    implementation("com.github.ajalt.clikt:clikt:5.0.1")
+    // Support for rendering markdown in help messages
+    implementation("com.github.ajalt.clikt:clikt-markdown:5.0.1")
+    // Logging
+    implementation("org.slf4j:slf4j-simple:2.0.3")
+    implementation("io.github.oshai:kotlin-logging-jvm:7.0.3")
+
     testImplementation(kotlin("test"))
 }
 
